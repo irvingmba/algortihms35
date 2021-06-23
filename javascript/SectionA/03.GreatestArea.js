27998c5fc0e7c544cdabfe1840cb0e39f66fef65
martin <imbedoya@tekmexico.com>
2021-06-22 19:05:30 -0700

    Solution of greatest area

--- javascript/SectionA/03.GreatestArea.js
+++ javascript/SectionA/03.GreatestArea.js
@@ -1,7 +1,63 @@
 function greatestArea(areaSet = []) {
   if (!areaSet || !Array.isArray(areaSet))
     throw new TypeError("Input a 2d array");
-    
+  let greatest = 0;
+  areaSet.reduce((acc, row) => {
+    const fused = fuseRows(acc, row);
+    const found = getArea(fused);
+    if (greatest < found) greatest = found;
+    return fused;
+  }, []);
+  return greatest;
+}
+
+function getArea(row) {
+  let area = 0;
+  const areas = new Map();
+  row.map((col) => {
+    if (col) {
+      if (areas.size === 0) {
+        areas.set(col, 1);
+        area = col;
+        return col;
+      }
+      if (areas.has(col)) {
+        areas.forEach((value, key) => {
+          if (col >= key) {
+            area = area < (value + 1) * key ? (value + 1) * key : area;
+            return areas.set(key, value + 1);
+          } else if (area < value * key) {
+            area = value * key;
+          }
+          areas.delete(key);
+        });
+      } else {
+        let largest = 0;
+        areas.forEach((value, key) => {
+          if (col >= key) {
+            area = area < (value + 1) * key ? (value + 1) * key : area;
+            return areas.set(key, value + 1);
+          } else if (area < value * key) {
+            area = value * key;
+          }
+          if (value > largest) largest = value;
+          areas.delete(key);
+        });
+        areas.set(col, largest + 1);
+        area = area < (largest + 1) * col ? (largest + 1) * col : area;
+      }
+    } else {
+      areas.clear();
+    }
+  });
+  return area;
+}
+
+function fuseRows(stored, row) {
+  return row.map((col, colIndex) => {
+    if (col) return stored[colIndex] ? stored[colIndex] + col : col;
+    else return 0;
+  });
 }
 
 module.exports = greatestArea;
--- javascript/SectionA/03.GreatestArea.test.js
+++ javascript/SectionA/03.GreatestArea.test.js
@@ -1,5 +1,77 @@
+const greatestArea = require("./03.GreatestArea");
+
 describe("Testing function to search greatest area", () => {
-    test("When it does not receive an array, it throws", () => {
-        
-    });
+  test("When it does not receive an array, it throws", () => {
+    expect(() => greatestArea({})).toThrow();
+  });
+
+  test("Simple find area example", () => {
+    const area = [
+      [0, 1, 1],
+      [0, 1, 1],
+      [1, 1, 0],
+    ];
+    const found = greatestArea(area);
+    expect(found).toBe(4);
+  });
+
+  test("Case 1", () => {
+    const area = [
+      [1, 0, 0, 0, 0],
+      [1, 1, 0, 0, 0],
+      [1, 1, 1, 0, 0],
+      [1, 1, 0, 0, 0],
+      [1, 0, 0, 0, 0],
+    ];
+    const found = greatestArea(area);
+    expect(found).toBe(6);
+  });
+
+  test("Case 2", () => {
+    const area = [
+      [0, 0, 0, 0, 1],
+      [0, 0, 0, 1, 1],
+      [0, 1, 1, 1, 1],
+      [0, 0, 0, 1, 1],
+      [0, 0, 0, 0, 1],
+    ];
+    const found = greatestArea(area);
+    expect(found).toBe(6);
+  });
+
+  test("Case 3", () => {
+    const area = [
+      [0, 0, 1, 0, 0],
+      [0, 1, 1, 1, 0],
+      [1, 1, 1, 1, 1],
+      [0, 1, 1, 1, 0],
+      [0, 0, 1, 0, 0],
+    ];
+    const found = greatestArea(area);
+    expect(found).toBe(9);
+  });
+
+  test("Case 3", () => {
+    const area = [
+      [1, 1, 0, 1, 1],
+      [1, 1, 0, 1, 1],
+      [1, 0, 0, 0, 1],
+      [1, 1, 0, 1, 1],
+      [1, 1, 0, 1, 1],
+    ];
+    const found = greatestArea(area);
+    expect(found).toBe(5);
+  });
+
+  test("Case 4", () => {
+    const area = [
+      [1, 1, 0, 0, 1],
+      [1, 1, 0, 1, 0],
+      [1, 1, 1, 1, 1],
+      [0, 1, 1, 0, 1],
+      [1, 0, 1, 1, 1],
+    ];
+    const found = greatestArea(area);
+    expect(found).toBe(6);
+  });
 });
