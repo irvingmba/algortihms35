d24a814a89e3d65cf5fd72197a79e2c51f9a13b0
martin <imbedoya@tekmexico.com>
2021-07-05 11:29:03 -0700

    Modification of algorithm 1A

--- javascript/SectionA/01.maxArrayValue.js
+++ javascript/SectionA/01.maxArrayValue.js
@@ -1,6 +1,11 @@
-function maxArrayValue(arr=[]){
-    if(!Array.isArray(arr)) throw new TypeError("Must provide an array");
-    return arr.reduce((prev,val)=>prev > val ? prev : val);
-};
+function maxArrayValue(arr, index = 0) {
+  if (!Array.isArray(arr)) throw new TypeError("Must provide an array");
+  if (index < arr.length - 1) {
+    const current = arr[index],
+      rest = maxArrayValue(arr, index + 1);
+    return current > rest ? current : rest;
+  }
+  return arr[index];
+}
 
 module.exports = maxArrayValue;
--- javascript/SectionA/01.maxArrayValue.test.js
+++ javascript/SectionA/01.maxArrayValue.test.js
@@ -7,13 +7,13 @@ describe("Testing function to get max value from array", () => {
     })
 
   test.each([{ input: {} }, { input: "string" }, { input: 12345 }])(
-    "If input is $in, it throws",
+    "If input is $input, it throws",
     ({ input }) => {
       expect(() => maxArrayValue(input)).toThrow();
     }
   );
 
-  test("It will return the max value froma array", () => {
+  test("It will return the max value from an array", () => {
     const max = maxArrayValue([5, 2, 7, 9, 1, 10]);
     expect(max).toBe(10);
   });
