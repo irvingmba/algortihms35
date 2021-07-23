8b8afa44afb847552b146d4685417e16fb5507f0
martin <imbedoya@tekmexico.com>
2021-07-23 10:12:23 -0700

    Fix algorithm A6

--- javascript/SectionA/06.MergeArray.js
+++ javascript/SectionA/06.MergeArray.js
@@ -1,17 +1,18 @@
 function mergeArray(firstArray, secondArray) {
   if (!Array.isArray(firstArray) || !Array.isArray(secondArray))
     throw new TypeError("You must input arrays");
-  let index = firstArray.length - secondArray.length - 1;
-  let pointer = firstArray.length - 1;
-  while (secondArray.length) {
+  let index = firstArray.length - secondArray.length - 1,
+    pointer = firstArray.length - 1,
+    pointerSec = secondArray.length - 1;
+  while (pointerSec != -1) {
     const fLast = firstArray[index],
-      sLast = secondArray[secondArray.length - 1];
+      sLast = secondArray[pointerSec];
     if (fLast > sLast) {
       firstArray[pointer] = fLast;
       --index;
     } else {
       firstArray[pointer] = sLast;
-      secondArray.pop();
+      --pointerSec;
     }
     --pointer;
   }
