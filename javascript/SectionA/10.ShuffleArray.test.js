ab0153efb045ce1de60e31241cdb9394687d9099
martin <imbedoya@tekmexico.com>
2021-08-09 11:48:24 -0700

    fix algorithms A10 A11

--- javascript/SectionA/10.ShuffleArray.test.js
+++ javascript/SectionA/10.ShuffleArray.test.js
@@ -2,25 +2,28 @@ const shuffleArray = require("./10.ShuffleArray");
 
 describe("Testing function to suffle an array", () => {
   test.each([{}, "string", 123])("When it gets %s, it throws", (input) => {
-      expect(()=>shuffleArray(input)).toThrow();
+    expect(() => shuffleArray(input)).toThrow();
   });
 
   test("Simple case", () => {
-      const sample = [1,2,3,4,5];
-      const shuffled = shuffleArray([...sample]);
-      expect(shuffled).not.toEqual(sample);
-  })
+    const sample = [1, 2, 3, 4, 5];
+    const shuffled = shuffleArray([...sample]);
+    expect(shuffled).not.toEqual(sample);
+    expect(shuffled).toHaveLength(sample.length);
+    expect(shuffled).toEqual(expect.arrayContaining(sample));
+  });
 
   test("Second case", () => {
-    const sample = [1,2,3,4,5,6,7,8,9,10,11,12];
+    const sample = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     const shuffled = shuffleArray([...sample]);
     expect(shuffled).not.toEqual(sample);
-})
+    expect(shuffled).toHaveLength(sample.length);
+    expect(shuffled).toEqual(expect.arrayContaining(sample));
+  });
 
-test("Mini case", () => {
-    const sample = [1,2];
+  test("Mini case", () => {
+    const sample = [1, 2];
     const shuffled = shuffleArray([...sample]);
-    expect(shuffled).not.toEqual(sample);
-})
-
+    expect(shuffled).toEqual([2, 1]);
+  });
 });
